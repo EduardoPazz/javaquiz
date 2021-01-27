706ffc5ec2b65b69d2f7dee97ce634b4aeaf6ae6
Eduardo da Paz <jooeduardo7@gmail.com>
2021-01-27 13:03:58 -0300

    Versão base do projeto

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,44 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import Footer from '../src/components/Footer'
+import GithubCorner from '../src/components/GitHubCorner'
+import QuizBackground from '../src/components/QuizBackground'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Titulo 1</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Conteúdo 1</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Header>
+            <h1>Titulo 2</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Conteúdo 2</p>
+          </Widget.Content>
+        </Widget>
+        <Footer></Footer>
+      </QuizContainer>
+      <GithubCorner projectUrl="https://github.com/EduardoPazz"></GithubCorner>
+    </QuizBackground>
+  )
 }
